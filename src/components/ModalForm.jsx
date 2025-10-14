505bc35981cfd9f9890cfdc8810361bf69a9e56c
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-10-14 18:02:08 +0530

    Add ModalForm component with initial fetch setup

---
+++ src/components/ModalForm.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+
+const ModalForm = ({id}) => {
+    const [fetchData, setFetchData] = useState([]);
+
+    useEffect(() =>{
+        const fetchInfo = async () => {
+            const res = await fetch()
+        }
+    }, [])
+  return (
+    <div className='modal-form'>
+      
+    </div>
+  )
+}
+
+export default ModalForm
