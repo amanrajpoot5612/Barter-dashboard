07d1911d32d5e0954bf6f4ce0042e5c39d9cb617
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-10-14 18:00:47 +0530

    Refactor App component structure and remove unused Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import Media from "./components/Media.jsx";
 import UploadMediaPage from "./pages/UploadMediaPage.jsx";
 import Navbar from './components/Navbar.jsx'
-import  Home from './components/Home.jsx'
+import  Page from './components/Page.jsx'
 import ContentPage from './components/ContentPages.jsx'
 import ImagePage from './components/ImagePage.jsx'
 import Testimonial from './components/Testimonial.jsx'
 import Footer from './components/Footer.jsx'
+import NavbarComponent from "./components/NavbarComponent.jsx";
+import FormComponent from "./components/FormComponent.jsx";
+
 
 
 const App = () => {
   return (
     <div className="app-component">
+      <NavbarComponent></NavbarComponent>
       <Navbar></Navbar>
-      <Home></Home>
-      <ContentPage></ContentPage>
+      {/* <FormComponent></FormComponent> */}
+      <Page></Page>
       <ImagePage></ImagePage>
       <Testimonial></Testimonial>
       <Footer></Footer>
-
-      {/* <Media></Media>
-      <UploadMediaPage></UploadMediaPage> */}
+      <UploadMediaPage></UploadMediaPage>
+      {/* <ImagePage></ImagePage> */}
+      {/* <Media></Media> */}
+      {/* <UploadMediaPage></UploadMediaPage> */}
     </div>
   );
 };
