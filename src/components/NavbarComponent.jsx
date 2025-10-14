ab3d10c1df1d6153ea6b676e6254675ed0ca7aeb
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-10-14 18:01:34 +0530

    Add NavbarComponent and associated styles for navigation

---
+++ src/components/NavbarComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './NavbarComponent.style.css'
+
+const NavbarComponent = () => {
+  return (
+    <nav>
+        <ul>
+          <li><a href="#navbar">Navbar</a></li>
+          <li><a href="#page">Page</a></li>
+          <li><a href="#image-page">Image page</a></li>
+          <li><a href="#testimonial">Testimonial</a></li>
+          <li><a href="#footer">Footer</a></li>
+        </ul>
+      </nav>
+  )
+}
+
+export default NavbarComponent
